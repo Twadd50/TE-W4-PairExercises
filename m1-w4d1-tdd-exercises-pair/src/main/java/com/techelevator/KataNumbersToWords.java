82b7ddbab19db84dfd15e2b74df0d6b03689e5ed
Tyler Waddington <twadd50@gmail.com>
2017-05-23 17:03:28 -0400

    day 2 exercises

--- m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataNumbersToWords.java
+++ m1-w4d1-tdd-exercises-pair/src/main/java/com/techelevator/KataNumbersToWords.java
@@ -1,39 +1,74 @@
 package com.techelevator;
 
 public class KataNumbersToWords {
-	
+
 	public String numberConverter(int number) {
-		
-		String zero = "zero";
-		String one = "one";
-		String two = "two";
-		String three = "three";
-		String four = "four";
-		String five = "five";
-		
-		if(number == 0){
-			return zero;
-		}else if(number == 1){
-			return one;
-		}else if(number == 2){
-			return two;
-		}else if(number == 3){
-			return three;
-		}else if(number == 4){
-			return four;
-		}else if(number == 5){
-			return five;
-		}else {
-			return "I can't count that far.";
+
+		String[] strOnes = { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven",
+				"Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", };
+
+		String[] strTens = { "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety",
+				"Hundred" };
+		String[] srHundreds = { "one hundred", "two hundred", "three hundred", "four hundred", "five hundred",
+				"six hundred", "seven hundred", "eight hundred", "nine hundred", "thousand", };
+
+		String hundredThousandsPlace = null;
+		String tenThousandsPlace = null;
+		String oneThousandsPlace = null;
+		String hundredsPlace = null;
+		String tensPlace = null;
+		String onesPlace = null;
+
+		if (number > 1000) {
+			int thousands = number / 1000;
+			int result1 = number - (number * 1000);
+			if (thousands > 100) {
+				int hundredThousands = thousands / 100;
+
+				if (hundredThousands <= srHundreds.length) {
+					hundredThousandsPlace = srHundreds[hundredThousands - 1];
+				}
+				int result2 = thousands - (hundredThousands * 100);
+				if (result2 > 10) {
+					int tenThousands = result2 / 10;
+					if (tenThousands <= strTens.length) {
+						tenThousandsPlace = strTens[tenThousands - 1];
+					}
+					int result3 = result2 - (tenThousands * 10);
+					int oneThousands = result3;
+					if (oneThousands <= strOnes.length) {
+						oneThousandsPlace = strOnes[oneThousands - 1];
+					}
+
+				}
+			}
+			if (result1 > 100) {
+				int hundreds = result1 / 100;
+				if (hundreds <= srHundreds.length) {
+					hundredsPlace = srHundreds[hundreds - 1];
+				}
+				int result4 = result1 - (hundreds * 100);
+				if (result4 > 10) {
+					int tens = result4 / 10;
+					if (tens <= strTens.length) {
+						tensPlace = strTens[tens - 1];
+					}
+					int ones = result4 - (tens * 10);
+					if (ones <= strOnes.length) {
+						onesPlace = strOnes[ones - 1];
+					}
+				}
+			}
+
 		}
-		
+
+		return hundredThousandsPlace + " " + tenThousandsPlace + " " + oneThousandsPlace + " " + hundredsPlace + " "
+				+ tensPlace + " " + onesPlace;
 	}
-	public int textConverter(String text){
-		
-		
-		
+
+	public int textConverter(String text) {
+
 		return 0;
 	}
 
-	
 }
