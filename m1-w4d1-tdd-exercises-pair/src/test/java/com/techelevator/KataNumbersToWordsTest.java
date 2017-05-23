d4921e7d58d877bbda6efa6437785a038dc248d7
Antoinne McKinney <antoinnemckinney@gmail.com>
2017-05-23 19:54:02 -0400

    Update Week4 Day 1 Pair

--- m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataNumbersToWordsTest.java
+++ m1-w4d1-tdd-exercises-pair/src/test/java/com/techelevator/KataNumbersToWordsTest.java
@@ -17,7 +17,42 @@ public class KataNumbersToWordsTest {
 	@Test
 	public void testConvertNumbersToText(){
 		
+		assertEquals("one hundred thirty two thousand five hundred thirty six", sut.numberConverter(132536));
+		
+	}
+	
+	@Test
+	public void testSpellZero(){
+		
 		assertEquals("zero", sut.numberConverter(0));
+		
+	}
+	
+	@Test
+	public void testSpellThirtyFive(){
+		
+		assertEquals("thirty five", sut.numberConverter(35));
+		
+	}
+	
+	@Test
+	public void testSpellTwoHundredSixtySeven(){
+		
+		assertEquals("two hundred sixty seven", sut.numberConverter(267));
+		
+	}
+	
+	@Test
+	public void testSpellThirtyFourThousandThreeHundredSeventyEight(){
+		
+		assertEquals("thirty four thousand three hundred seventy eight", sut.numberConverter(34378));
+		
+	}
+	
+	@Test
+	public void testSpellFiveThousand(){
+		
+		assertEquals("five thousand", sut.numberConverter(5000));
 		
 	}
 	
