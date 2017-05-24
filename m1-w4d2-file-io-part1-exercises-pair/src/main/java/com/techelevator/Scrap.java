22e839c0d9ad1c6e8e719c8b86984cf81573a4d8
Tyler Waddington <twadd50@gmail.com>
2017-05-24 14:41:07 -0400

    Finished day 2 exercises (word counter)

---
+++ m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/Scrap.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+public class Scrap {
+
+	
+	
+	
+	while ((line = br.readLine()) != null) {
+	
+	
+	
+	
+	int sentenceCount = 0;
+	int lastIndex = 0; 
+	{
+	for(int i=0;i<AliceWonderland.length();i++){  
+	    for(int j=0;j<SENTENCE_DELIMETERS.length();j++){  
+	        if(myFile.charAt(i)==SENTENCE_DELIMETERS.charAt(j)){
+	            if(lastIndex!=i-1){
+	                sentenceCount++;
+	            }
+	            lastIndex=i;
+	        }
+	    }
+	}
+}
+
+	
+	int count = str.split("[!?.:]+").length;
+	
+	
+    String violet;
+  while(Compton.ready())
+  {
+      violet=Compton.readLine();
+      sentenceCount=violet.split("[!?.:]+").length;
+      System.out.println("the number of words in line is " + 
+
+        sentenceCount);
+   }
+  
+  String[] words = line.split("\\.")
+		  
+		  
+		  String[] words = line.split(Pattern.quote("."));
--- m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
+++ m1-w4d2-file-io-part1-exercises-pair/src/main/java/com/techelevator/WordCount.java
@@ -2,12 +2,55 @@ package com.techelevator;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class WordCount {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader docReader     = new BufferedReader(new FileReader("alices_adventures_in_wonderland.txt"));
+		File     file 			     = new File("alices_adventures_in_wonderland.txt");
+		int      wordCount 		     = 0;
+		int      sentenceCount		 = 0;
+		String   sentenceDelimeters	 = "[?!\\.]";
+		String   wordDelimeters 	 = " +";
+		String   lineReader 	     = docReader.readLine();
+
+		try (Scanner sc = new Scanner(new FileInputStream(file))) {
+			while (sc.hasNext()) {
+				lineReader = sc.nextLine();
+					//sc.next();
+					wordCount += lineReader.split(wordDelimeters).length;
+					
+				
+				
+			}
+
+		} catch (FileNotFoundException missingFile) {
+			System.out.println(missingFile.getMessage());
+
+		}
+
+		try (Scanner sc = new Scanner(new FileInputStream(file))) {
+			while (sc.hasNext()) {
+				lineReader = sc.nextLine();
+					sentenceCount += lineReader.split(sentenceDelimeters).length;
+					sentenceCount--;
+				
+			
+			}
+
+		} catch (FileNotFoundException missingFile) {
+			System.out.println(missingFile.getMessage());
+
+		}
+		docReader.close();
+		System.out.println("Number of words: " + wordCount);
+		System.out.println("Number of sentences:" + sentenceCount);
 
 	}
 
